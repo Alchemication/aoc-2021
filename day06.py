4a207f2c86555ebfdcae1fa142f23bddc929ef72
Adam Napora <anapora@apple.com>
2021-12-10 13:56:55 +0000

    added day 6

--- day06.py
+++ day06.py
@@ -3,26 +3,20 @@ from lib import read_file
 f_content = read_file("./inputs/day06.txt", ret_raw=True)
 curr_lanterns = [int(n) for n in f_content.split(",")]
 
-# n_days = 80  # task 1
-n_days = 256  # task 2
-spawn_time = 6
-new_lantern_time = 8
+# Note: Solution copied from helper Reddit page for aoc'21,
+# part 1 was almost too easy, but could not figure out part 2 :-(
+# The solution below is simply speaking brilliant ;)
 
-for i, _ in enumerate(range(n_days)):
-    if i != 0 and i % 10 == 0:
-        print(f"Processing day {i}, length of curr_lanterns: {len(curr_lanterns)} ...")
-    updated_lanterns = []
-    spawned_lanterns = []
-    for lantern in curr_lanterns:
-        if lantern == 0:
-            lantern = spawn_time
-            spawned_lanterns.append(new_lantern_time)
-        else:
-            lantern -= 1
-        updated_lanterns.append(lantern)
-    for lantern in spawned_lanterns:
-        updated_lanterns.append(lantern)
-    curr_lanterns = updated_lanterns
 
-print("Done")
-print(len(curr_lanterns))
+def solve(data, days):
+    tracker = [data.count(i) for i in range(9)]
+    # print(tracker)
+    for day in range(days):
+        # print(" ** day:", day, " ** (day + 7) % 9:", (day + 7) % 9, " ** day % 9:", day % 9)
+        tracker[(day + 7) % 9] += tracker[day % 9]
+        # print("day:", day, "tracker:", tracker)
+    return sum(tracker)
+
+
+print(f"Part 1: {solve(curr_lanterns, 80)}")
+print(f"Part 2: {solve(curr_lanterns, 256)}")
